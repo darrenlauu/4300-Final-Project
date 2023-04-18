0588047d53eb3d7ab2cd9983214c8c713f5f7b9d
Zachary Ross <znr3@cornell.edu>
2023-04-17 22:05:15 -0400

    fixed password to os

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "password"
+MYSQL_USER_PASSWORD = os.environ.get("MYSQL_ROOT_PASSWORD")
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "kardashiandb"
 
